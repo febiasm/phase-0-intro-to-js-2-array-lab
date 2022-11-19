2f694fc79cff4c93c982fd9fe778c24a10ad8156
febiasm <trevorfebias@gmail.com>
2022-11-19 22:56:08 +0300

    done with assign

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+function beforeEach(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+function destructivelyAppendCat(name){
+    beforeEach()
+    cats.push(name)
+}
+destructivelyAppendCat("Ralph")
+function destructivelyPrependCat(name){
+    beforeEach()
+    cats.unshift(name)
+}
+destructivelyPrependCat("Bob")
+function destructivelyRemoveLastCat(){
+    beforeEach()
+    cats.pop()
+}
+destructivelyRemoveLastCat()
+function destructivelyRemoveFirstCat(){
+    beforeEach()
+    cats.shift()
+}
+destructivelyRemoveFirstCat()
+function appendCat(name){
+    beforeEach()
+    return [...cats, name]
+}
+
+appendCat("Broom")
+
+function prependCat(name){
+    beforeEach()
+    return [name, ...cats]
+}
+prependCat("Arnold")
+prependCat("Arnold");
+
+function removeLastCat(){
+    beforeEach()
+    return cats.slice(0,2)
+}
+removeLastCat()
+function removeFirstCat(){
+    beforeEach()
+    return cats.slice(1)
+}
+ removeFirstCat()
